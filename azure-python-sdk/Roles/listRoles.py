54f4e4a62d70b19ba58b766cb7438840f389981e
Paul Aiton <paul@aiton.info>
2020-10-12 00:05:46 -0700

    blah

--- azure-python-sdk/Roles/listRoles.py
+++ azure-python-sdk/Roles/listRoles.py
@@ -1,6 +1,4 @@
-#/usr/bin/env python
-
-#import azure
+#!/usr/bin/env python
 from azure.common.client_factory import get_client_from_cli_profile
 
 from azure.mgmt.resource.subscriptions import SubscriptionClient
@@ -8,21 +6,23 @@ from azure.mgmt.resource.subscriptions import SubscriptionClient
 from azure.mgmt.resource import ResourceManagementClient
 
 from azure.mgmt.authorization import AuthorizationManagementClient
-subscription_id=''
+subscription_id = ''
 
 if __name__ == "__main__":
-    authorization_client = get_client_from_cli_profile(AuthorizationManagementClient) #, subscription_id=subscription_id )
+    authorization_client = get_client_from_cli_profile(AuthorizationManagementClient)  # , subscription_id=subscription_id )
 
-    role_list = authorization_client.role_assignments.list( filter=None, custom_headers=None, raw=False)
-    #role_get = authorization_client.role_assignments.get( scope='', role_assignment_name='', custom_headers=None, raw=False)
-    #role_list_for_resource_group = authorization_client.role_assignments.list_for_resource_group(resource_group_name='', filter=None, custom_headers=None, raw=False)
-    #role_list_for_scope = authorization_client.role_assignments.list_for_scope(scope='', filter=None, custom_headers=None, raw=False)
-    #role_get_by_id = authorization_client.role_assignments.get_by_id(role_id='', custom_headers=None, raw=False)
-    #role_list_for_resource = authorization_client.role_assignments.list_for_resource(resource_group_name='', resource_provider_namespace='', parent_resource_path='',
-                                                                                     #resource_type='', resource_name='', filter=None, custom_headers=None, raw=False)
-    
+    role_list = authorization_client.role_assignments.list(filter=None, custom_headers=None, raw=False)
+    role_get = authorization_client.role_assignments.get(scope='', role_assignment_name='', custom_headers=None, raw=False)
+    role_list_for_resource_group = authorization_client.role_assignments.list_for_resource_group(resource_group_name='', filter=None, custom_headers=None, raw=False)
+    role_list_for_scope = authorization_client.role_assignments.list_for_scope(scope='', filter='atScope()', custom_headers=None, raw=False)
+    role_get_by_id = authorization_client.role_assignments.get_by_id(role_id='', custom_headers=None, raw=False)
+    role_list_for_resource = authorization_client.role_assignments.list_for_resource(resource_group_name='', resource_provider_namespace='', parent_resource_path='',
+                                                                                     resource_type='', resource_name='', filter=None, custom_headers=None, raw=False)
 
-    for role_assignment in role_list:
+    # for role_assignment in role_list:
+    # for role_assignment in [ role_get ]:
+    print('########################################################')
+    for role_assignment in role_list_for_scope:
         print('id: {}'.format(role_assignment.id))
         print('name: {}'.format(role_assignment.name))
         print('principal_id: {}'.format(role_assignment.principal_id))
