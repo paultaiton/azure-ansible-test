f7862731413e28d1a7d91ec452fc2f0ce94da556
Paul Aiton <paul@aiton.info>
2020-10-28 13:04:50 -0700

    list of subs

--- azure-python-sdk/Storage/listBlobContainers.py
+++ azure-python-sdk/Storage/listBlobContainers.py
@@ -3,19 +3,19 @@ import uuid
 from azure.common.client_factory import get_client_from_cli_profile
 
 from azure.mgmt.resource.subscriptions import SubscriptionClient
-
 from azure.mgmt.storage import StorageManagementClient
 
-subscription_id = ''
+subscription_names = ["subscription-name"]
 
 if __name__ == "__main__":
-    storage_client = get_client_from_cli_profile(StorageManagementClient)  # , subscription_id=subscription_id )
+    subscription_client = get_client_from_cli_profile(SubscriptionClient)
 
-    storage_list = storage_client.storage_accounts.list()
-
-    print('########################################################')
-    for storage_account in storage_list:
-        resource_group_name = storage_account.id.split('/')[4]
-        for blob_container in storage_client.blob_containers.list(resource_group_name=resource_group_name, account_name=storage_account.name):
-            if blob_container.public_access:
-                print(blob_container.id)
+    for subscription in subscription_client.subscriptions.list():
+        if subscription.display_name in subscription_names:
+            print('###########' + subscription.display_name + '###############')
+            storage_client = get_client_from_cli_profile(StorageManagementClient, subscription_id=subscription.subscription_id)
+            for storage_account in storage_client.storage_accounts.list():
+                resource_group_name = storage_account.id.split('/')[4]
+                for blob_container in storage_client.blob_containers.list(resource_group_name=resource_group_name, account_name=storage_account.name):
+                    if blob_container.public_access:
+                        print(blob_container.id)
