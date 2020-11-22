6b4c194f0468fd816e10391ea58ff59b0b88dcb7
Paul Aiton <paul@aiton.info>
2020-11-22 16:49:23 -0700

    f

--- azure-python-sdk/Storage/playgroundStorageAccount.py
+++ azure-python-sdk/Storage/playgroundStorageAccount.py
@@ -10,49 +10,52 @@ from msrestazure.azure_exceptions import CloudError
 
 
 if __name__ == "__main__":
-    model_parameters = dict(
 
-    )
-
-    storage_account_parameters = dict(
-        # access_tier = kwargs.get('access_tier', None),
-        # custom_domain = kwargs.get('custom_domain', None),
-        # enable_https_traffic_only = kwargs.get('enable_https_traffic_only', False),
-        # encryption = kwargs.get('encryption', None),
-        # identity = kwargs.get('identity', None),
-        # is_hns_enabled = kwargs.get('is_hns_enabled', False)
-        # kind = kwargs.get('kind', None),
-        # location = kwargs.get('location', None),
-        # network_rule_set = kwargs.get('network_rule_set', None),
-        # sku = kwargs.get('sku', None),
-        # tags = kwargs.get('tags', None),
+    storage_parameters_dict = dict(
+        access_tier='Hot',
+        # custom_domain=kwargs.get('custom_domain', None),
+        # enable_https_traffic_only=kwargs.get('enable_https_traffic_only', False),
+        # encryption=kwargs.get('encryption', None),
+        # identity=kwargs.get('identity', None),
+        # is_hns_enabled=kwargs.get('is_hns_enabled', False)
+        kind='StorageV2',
+        location='westus',
+        # network_rule_set=kwargs.get('network_rule_set', None),
+        sku={'name': 'Standard_GRS'},
+        tags={'appcode': 'esia'}
     )
 
     storage_client = get_client_from_cli_profile(StorageManagementClient)
     storage_models = StorageManagementClient.models("2019-06-01")
 
-    storage_name = 'storagename'
-    resource_group_name = 'resource-group-name'
+    storage_name = 'paulsstorageaccount'
+    resource_group_name = 'resource-group-01'
 
     storage_accounts = []
-    try:
-        storage_accounts = storage_client.storage_accounts.list()
-        # storage_accounts = [storage_client.storage_accounts.get_properties(resource_group_name=resource_group_name, account_name=storage_name)]
-        # storage_accounts = [storage_client.storage_accounts.create(resource_group_name=resource_group_name, account_name=storage_name, parameters)]
-        # storage_accounts = storage_client.storage_accounts.delete()
-        # storage_accounts = storage_client.storage_accounts.failover()
-        # storage_accounts = storage_client.storage_accounts.list_account_sas()
-        # storage_accounts = storage_client.storage_accounts.list_by_resource_group()
-        # storage_accounts = storage_client.storage_accounts.list_keys()
-        # storage_accounts = storage_client.storage_accounts.list_service_sas()
-        # storage_accounts = storage_client.storage_accounts.regenerate_key()
-        # storage_accounts = storage_client.storage_accounts.restore_blob_ranges()
-        # storage_accounts = storage_client.storage_accounts.revoke_user_delegation_keys()
-        # storage_accounts = storage_client.storage_accounts.update()
-    except CloudError:
-        pass
+    name_check_response = None
     if not storage_accounts:
         name_check_response = storage_client.storage_accounts.check_name_availability(name=storage_name)
+    if name_check_response and name_check_response.name_available:
+        storage_parameters = StorageAccountCreateParameters(**storage_parameters_dict)
+        poller = storage_client.storage_accounts.create(resource_group_name=resource_group_name, account_name=storage_name, parameters=storage_parameters_dict)
+        while not poller.done():
+            self.log("Waiting for {0} sec".format(10))
+            poller.wait(10)
+
+    # try:
+    # storage_accounts = storage_client.storage_accounts.list()
+    storage_accounts = [storage_client.storage_accounts.get_properties(resource_group_name=resource_group_name, account_name=storage_name)]
+    # storage_accounts = storage_client.storage_accounts.delete()
+    # storage_accounts = storage_client.storage_accounts.failover()
+    # storage_accounts = storage_client.storage_accounts.list_account_sas()
+    # storage_accounts = storage_client.storage_accounts.list_by_resource_group()
+    # storage_account_keys = storage_client.storage_accounts.list_keys(resource_group_name=resource_group_name, account_name=storage_name)
+    # storage_accounts = storage_client.storage_accounts.list_service_sas()
+    # storage_accounts = storage_client.storage_accounts.regenerate_key()
+    # storage_accounts = storage_client.storage_accounts.restore_blob_ranges()
+    # storage_accounts = storage_client.storage_accounts.revoke_user_delegation_keys()
+    # storage_accounts = storage_client.storage_accounts.update()
+    # except CloudError:
 
     for account in storage_accounts:
         account_dict = account.as_dict()
