10b84f386a007c2c24c9b234321b95559471b451
Paul Aiton <paul@aiton.info>
2021-01-24 10:40:35 -0700

    asdf

---
+++ azure-python-sdk/ManagementGroup/listManagementGroups.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from azure.common.client_factory import get_client_from_cli_profile
+from azure.mgmt.resource.subscriptions import SubscriptionClient
+from azure.mgmt.managementgroups import ManagementGroupsClient
+
+if __name__ == "__main__":
+    subscription_client = get_client_from_cli_profile(SubscriptionClient)
+    subscription_client = get_client_from_cli_profile(ManagementGroupsClient)
+
+    # subscription_list = [subscription_client.subscriptions.get('')]
+    subscription_list = subscription_client.subscriptions.list()
+    # subscription_list = subscription_client.subscriptions.list_locations()
+    subscription_listraw = list(subscription_client.subscriptions.list())
+
+    for sub in subscription_list:
+        print('display_name: {}'.format(sub.display_name))
+        print('id: {}'.format(sub.id))
+        print('state: {}'.format(sub.state))
+        print('subscription_id: {}'.format(sub.subscription_id))
+        if sub.tags:
+            print('tags: ')
+            for key in sub.tags:
+                print('  {}: {}'.format(key, sub.tags.get(key)))
+        print('tenant_id: {}'.format(sub.tenant_id))
+        print('')
+    print('Hello world!')
