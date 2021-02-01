7df65f5241580263f29409d5d6c978a8025808e3
Paul Aiton <paul@aiton.info>
2021-02-01 15:24:01 -0700

    asdf

--- azure-python-sdk/ManagementGroup/listManagementGroups.py
+++ azure-python-sdk/ManagementGroup/listManagementGroups.py
@@ -8,21 +8,15 @@ from msrestazure.azure_active_directory import UserPassCredentials
 def main():
     subscription_client = get_client_from_cli_profile(SubscriptionClient)
     management_groups_api = ManagementGroupsAPI(subscription_client.config.credentials)
+    print('')
 
     for group in management_groups_api.management_groups.list():
-        # print('{}'.format(group.display_name))
+        print('display_name: {}'.format(group.display_name))
+        print('id: {}'.format(group.id))
+        print('name: {}'.format(group.name))
+        print('tenant_id: {}'.format(group.tenant_id))
+        print('type: {}'.format(group.type))
         print('')
-    print('Hello world!')
-
-
-def get_entities(tenant_id=None):
-    credentials = get_credential(tenant_id)
-
-    mgmt_groups_api = ManagementGroupsAPI(credentials)
-    entities = mgmt_groups_api.entities.list()
-    entity_infos = [entity for entity in entities]
-    entity_names = [entity.display_name for entity in entity_infos]
-    print(f'    entities: {entity_names}')
 
 
 if __name__ == '__main__':
