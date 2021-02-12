a39475f83a91aacc95e9caf3cd831247f41122f6
Paul Aiton <paul@aiton.info>
2021-02-12 10:30:24 -0700

    fdsa

--- azure-python-sdk/ManagementGroup/listManagementGroups.py
+++ azure-python-sdk/ManagementGroup/listManagementGroups.py
@@ -4,8 +4,8 @@ from azure.mgmt.resource import SubscriptionClient
 from azure.mgmt.managementgroups import ManagementGroupsAPI
 from msrestazure.azure_active_directory import UserPassCredentials
 
-group_name = 'ent-it'
-group_id = '/providers/Microsoft.Management/managementGroups/ent-it'
+group_name = ''
+group_id = ''
 
 
 def main():
@@ -13,11 +13,12 @@ def main():
     management_groups_api = ManagementGroupsAPI(subscription_client.config.credentials)
     print('')
 
-    # name = group_name
-    name = group_id.split('/')[-1]
+    name = group_name
+    # name = group_id.split('/')[-1]
     # management_group_list = management_groups_api.management_groups.list()
-    management_group_list = [management_groups_api.management_groups.get(group_id=name, expand='children', recurse=True)]
+    # management_group_list = [management_groups_api.management_groups.get(group_id=name, expand='', recurse=False)]
     # management_group_list = [management_groups_api.management_groups.get(group_id=name, expand='children', recurse=False)]
+    management_group_list = [management_groups_api.management_groups.get(group_id=name, expand='children', recurse=True)]
     for group in management_group_list:
         print('display_name: {}'.format(group.display_name))
         print('id: {}'.format(group.id))
