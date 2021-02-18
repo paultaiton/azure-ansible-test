8964769c8560382694e6e443816ca08b8f90bb98
Paul Aiton <paul@aiton.info>
2021-02-18 16:07:09 -0700

    a

--- azure-python-sdk/Storage/listNoncompliantBlobContainers.py
+++ azure-python-sdk/Storage/listNoncompliantBlobContainers.py
@@ -7,7 +7,30 @@ from azure.mgmt.storage import StorageManagementClient
 from msrestazure.tools import parse_resource_id
 from msrestazure.azure_exceptions import CloudError
 
-subscription_names = ["subscription-name"]
+subscription_names = [
+    "az-core-nonprod-01",
+    "az-core-prod-01",
+    "az-entaks-nonprod-01",
+    "az-entaks-nonprod-02",
+    "az-entaks-prod-01",
+    "az-entaks-prod-02",
+    "az-entapp-nonprod-01",
+    "az-entapp-prod-01",
+    "az-entcore-prod-01",
+    "az-imvd-prod-01",
+    "az-metrc-nonprod-01",
+    "az-pciapp-nonprod-01",
+    "az-pciapp-prod-01",
+    "az-phiapp-nonprod-01",
+    "az-phiapp-prod-01",
+    "az-resapp-nonprod-01",
+    "az-resapp-prod-01",
+    "az-resibm-nonprod-01",
+    "az-resibm-prod-01",
+    "az-resmetrc-nonprod-01",
+    "az-resmetrc-prod-01"
+]
+
 
 if __name__ == "__main__":
     subscription_client = get_client_from_cli_profile(SubscriptionClient)
@@ -31,8 +54,9 @@ if __name__ == "__main__":
                     resource_group_name = parse_resource_id(storage_account.id).get('resource_group')
                     for blob_container in storage_client.blob_containers.list(resource_group_name=resource_group_name,
                                                                               account_name=storage_account.name):
-                        if blob_container.public_access != 'None':
+                        if blob_container.public_access and blob_container.public_access != 'None':
                             print(blob_container.id)
+                            print("Blob Public Access: {}".format(blob_container.public_access))
                 except CloudError as error:
                     print('Azure Cloud Error: {}'.format(error))
                     sleep(5)
